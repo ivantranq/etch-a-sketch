6db764ea0b7aa15858f9261721134fc356cfaa9c
Ivan <ivan.tranquilan@outlook.com>
2023-11-17 08:03:03 +1030

    Title added and 16x16 grid added through through script

---
+++ script.js
@@ -0,0 +1,11 @@
+const container = document.querySelector('.container');
+
+
+
+for (let i = 0; i < 16; i++){
+  for (let j = 0; j < 16; j++) {
+    let square = document.createElement('div');
+    square.classList.add('square');
+    container.appendChild(square);
+  }
+};
