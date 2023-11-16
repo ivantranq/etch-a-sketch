fc51a63c79a8577db85d5daee03951f0f2ebb312
Ivan <ivan.tranquilan@outlook.com>
2023-11-17 08:16:02 +1030

    Hover effect working, reset button configurated

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 const container = document.querySelector('.container');
+const btn = document.querySelector('.reset');
 
 
 
 for (let i = 0; i < 16; i++){
   for (let j = 0; j < 16; j++) {
-    let square = document.createElement('div');
+    const square = document.createElement('div');
     square.classList.add('square');
     container.appendChild(square);
   }
 };
+
+const allSquares = document.querySelectorAll('.square');
+
+allSquares.forEach(square => {
+  square.addEventListener('mouseover', () => {
+    console.log('Square being hovered over.')
+    square.classList.add('hover');
+  })
+});
+
+btn.addEventListener('click', () => {
+  allSquares.forEach(square => {
+    square.classList.remove('hover');
+  })
+})
